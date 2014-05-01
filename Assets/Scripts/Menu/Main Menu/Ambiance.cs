5badb22c392d7e6f456b58c00a54a02eaa05d11a
Thomas Rouvinez <hadron.anomaly.dev@gmail.com>
2014-05-01 13:26:36 +0200

    Ready for gameplay pass

--- Assets/Scripts/Menu/Main Menu/Ambiance.cs
+++ Assets/Scripts/Menu/Main Menu/Ambiance.cs
@@ -3,32 +3,17 @@ using System.Collections;
 
 public class Ambiance : MonoBehaviour {
 
+	// Your light gameObject here.
 	public Light light;
-	private float minTime = .5f;
-	private float threshold = .5f;
-	private float lastTime = .0f;
-	private float[] smoothing = new float[20];
-
-	void Start(){
-		// Initialize the array.
-		for(int i = 0 ; i < smoothing.Length ; i++){
-			smoothing[i] = .0f;
-		}
-	}
+	short delay = 0;
 
 	void Update () {
-		float sum = .0f;
-		
-		// Shift values in the table.
-		for(int i = 1 ; i < smoothing.Length ; i++)
-		{
-			smoothing[i-1] = smoothing[i];
-			sum+= smoothing[i-1];
-		}
-		
-		smoothing[smoothing.Length -1] = Random.value;
-		sum+= smoothing[smoothing.Length -1];
+		delay++;
 
-		light.intensity = sum / smoothing.Length;
+		if(delay > 8)
+		{
+			light.intensity = Random.value;
+			delay = -0;
+		}
 	}
 }
